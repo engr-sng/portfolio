7d4209d2d67b99bcd92dc008cf36def83b60394e
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2022-10-01 12:56:21 +0900

    [add]スクロール機能追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 $(function() {
+    var headerHeight = $(".header-wrapper").height();
+    $("body").css("margin-top", headerHeight);
+
+    var navHeight = $(".header-wrapper").outerHeight();
+    $('a[href^="#"]').on("click", function () {
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? "html" : href);
+      var position = target.offset().top - navHeight;
+      $("html, body").animate({ scrollTop: position, }, 600, "swing");
+      return false;
+    });
+  
+    $("#js-page-top").on("click", function () {
+      $("body,html").animate({ scrollTop: 0, }, 600);
+      return false;
+    });
+
     $('#scroll_back a').on('click',function(event){
         $('body, html').animate({
         scrollTop: 0
-    }, 800);
+    }, 600);
         event.preventDefault();
     });
 
